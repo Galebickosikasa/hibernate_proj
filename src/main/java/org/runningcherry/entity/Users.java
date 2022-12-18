6c3ab1250cc8e7af75460c6b739d6e5ccf3003bc
Galebickosikasa <glebkolyshev@gmail.com>
2022-12-18 13:41:40 +0200

    add Users table mapping to entity

---
+++ src/main/java/org/runningcherry/entity/Users.java
@@ -0,0 +1,45 @@
+package org.runningcherry.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+public class Users {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+    @Basic
+    @Column(name = "username", nullable = false, length = 25)
+    private String username;
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Users users = (Users) o;
+        return userId == users.userId && Objects.equals(username, users.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, username);
+    }
+}
